9092cb5c45006dffd834f6d7478b99f836c8d59e
Yashavanth <Yashavanth.hsn@gmail.com>
2015-01-06 16:35:01 +0100

    [Logger] Refinement

--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -1,12 +1,12 @@
-
 #include<iostream>
 #include<ctime>
 #include<stdlib.h>
-
+#include "../Queue/FifoQueue.h"
 #include "Logger.h"
 
 std::shared_ptr<Logger> Logger::_pInstance = NULL;
 std::once_flag Logger::once;
+std::atomic<bool> Logger::stopSignal;
 
 Logger::Logger() {
 
@@ -39,33 +39,30 @@ Logger::~Logger() {
     _pInstance->logFileStream.close();
     _pInstance = NULL;
   }
+ //TODO clear all messages in all queues and deallocate all queues.
 }
 
 std::shared_ptr<Logger> Logger::getLogger() {
 
   std::string path = "/home/Documents/log.txt";
-
   std::call_once( Logger::once, [] () {
                   Logger::_pInstance.reset( new Logger()); });
-
   return Logger::_pInstance;
 }
 
 std::string Logger::getLogFile() const {
-
   return this->logFilePath;
 }
 
 void Logger::setLogFile(std::string s) {
-
   if(!s.empty()){
     this->logFilePath = s;
   }
 }
 
-void Logger::logWrite(std::string s) {
-  if(!s.empty()) {
-    this->logFileStream << Logger::getDate() << "\n";
+void Logger::logWrite(std::string* s) {
+  if(!s->empty()) {
+    this->logFileStream << Logger::getDate() << *s << '\n';
     this->logFileStream.flush();
   }
 }
@@ -77,5 +74,82 @@ std::string Logger::getDate() {
   std::string tme(tm);
   tme.erase(tme.length()-1);
   return tme;
+}
+
+FifoQueue<std::string, MAX_MESSAGES>* Logger::subScribe() {
+  FifoQueue<std::string, MAX_MESSAGES> * toAdd = new FifoQueue<std::string, MAX_MESSAGES>();
+  this->sreqs++;
+  std::lock_guard<std::mutex> guard(this->logLock);
+  this->subscribers.push_back(toAdd);
+  this->sreqs--;
+  return toAdd;
+}
+
+void Logger::unSubscribe(FifoQueue<std::string, MAX_MESSAGES> *toDel) {
+  std::cout << "unSubscribe called here " << std::endl;
+  if (toDel == NULL)
+    return;
+
+  this->usreqs++;
+  std::lock_guard<std::mutex> guard(this->logLock);
+  std::vector<FifoQueue<std::string, MAX_MESSAGES>*>::iterator it;
+  it = std::find(this->subscribers.begin(), this->subscribers.end(), toDel);
+  this->usreqs--;
+  if(it == this->subscribers.end()) {
+    return;
+  } else {
+    std::string* s;
+    while ((s = (*it)->pop())!= NULL ) {
+          this->logWrite(s);
+          delete s;
+    }
+    delete *it;
+    this->subscribers.erase(it);
+  }
+}
+
+void Logger::stop() {
+  std::cout<<"Stop invoked  " << std::endl;
+  this->stopSignal.store(true);
+}
+
+void Logger::start() {
+
+  this->stopSignal.store(false);
+  std::vector<FifoQueue<std::string, MAX_MESSAGES>*>::iterator it;
+  std::string* s;
+  std::cout<< "Logger::Start called " << std::endl;
+  while(!this->stopSignal.load()) {
+    this->logLock.lock();
+    for(it = this->subscribers.begin(); it != this->subscribers.end(); it++) {
+      s = (*it)->pop();
+      if(s != NULL) {
+        this->logWrite(s);
+        delete s;
+      }
+    }
+
+    this->logLock.unlock();
+    if(this->sreqs > 0 || this->usreqs > 0) {
+      std::cout <<" Handling the sub and unsub requests " << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+
+  }
+
+  std::cout << "start to clear up " << std::endl;
+    /* delete everything before joining main thread */
+  this->logLock.lock();
+  for(it = this->subscribers.begin(); it != this->subscribers.end(); ++it) {
+    while((s = (*it)->pop()) != NULL ) {
+      this->logWrite(s);
+      delete s;
+    }
+    delete *it;
+  }
+  this->logLock.unlock();
+  return;
 }
 
--- src/Logger/Logger.h
+++ src/Logger/Logger.h
@@ -4,7 +4,13 @@
 #include<fstream>
 #include<mutex>
 #include<memory>
+#include<vector>
+#include<algorithm>
+#include<chrono>
+#include<thread>
+#include "../Queue/FifoQueue.h"
 
+#define MAX_MESSAGES 40
 /* This class provides logging capability to a file.
    It uses singleton design pattren, so that only one
    instance of the Logger is present throughout the
@@ -19,21 +25,31 @@ class Logger {
     std::ofstream logFileStream;
     static std::shared_ptr<Logger> _pInstance;
     static std::once_flag once;
+    static std::atomic<bool> stopSignal;
 
     /* Make all constructors private */
     Logger();
     Logger(const Logger&);
     Logger & operator = (const Logger&);
-
+    /* an array of all subscribers queues */
+    std::vector<FifoQueue<std::string, MAX_MESSAGES>*> subscribers;
+    /* Mutex for array */
+    std::mutex logLock;
+    std::atomic<size_t> sreqs;
+    std::atomic<size_t> usreqs;
   public:
     ~Logger();
     std::ofstream getLogStream();
     void setLogFile(std::string);
     std::string getLogFile() const;
-    void logWrite(std::string);
+    void logWrite(std::string*);
     static std::string getDate();
     /* Public  Method to get Instance */
     static std::shared_ptr<Logger> getLogger();
+    FifoQueue<std::string, MAX_MESSAGES>* subScribe();
+    void unSubscribe(FifoQueue<std::string, MAX_MESSAGES>*);
+    void start();
+    void stop();
 };
 
 #endif
